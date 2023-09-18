5bf5fca8032dd4d889a221f2cdb7fdce193fe3b9
Ianzani <ianzanisirbone@gmail.com>
2023-09-18 10:46:42 -0300

    Trabalho 1 created and main.c added

---
+++ Trabalho1/main.c
@@ -0,0 +1,34 @@
+/*
+Author: Ian Zaniolo Sirbone
+Title: SEL0456 Project 1
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+void sqrt2(double x){
+    printf("Raiz de %.2f: %.2f\n", x, sqrt(x));
+}
+
+void pow2(double x){
+    printf("%.2f elevado à 2: %.2f\n", x, pow(x, 2));
+}
+
+void div2(double x){
+    printf("%.2f dividido por 2: %.2f\n", x, x/2);
+}
+
+int main(void){
+
+    double array[] = {2, 4, 6, 8, 10};
+
+    int i;
+    for (i=0; i<sizeof(array)/sizeof(double); i++){
+        sqrt2(array[i]);
+        pow2(array[i]);
+        div2(array[i]);
+        printf("\n");
+    }
+
+    return 0;
+}
