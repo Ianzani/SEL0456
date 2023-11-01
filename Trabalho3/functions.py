91c5189732a7fac76f25ec124ced32b2b67e5f3a
Ianzani <ianzanisirbone@gmail.com>
2023-11-01 20:38:44 -0300

    Trying to use python application

--- Trabalho3/functions.py
+++ Trabalho3/functions.py
@@ -4,13 +4,13 @@ from hashlib import sha256
 def hash_password(password) -> str:
     return sha256(password.encode('utf-8')).hexdigest()
 
-def verify_password() -> bool:
+def verify_password(password_hash_dir, password_entry_dir) -> bool:
 
-    with open('password_entry.txt', 'r') as file:
+    with open(password_entry_dir, 'r') as file:
         for line in file:
             password = line
 
-    with open('password_hash.txt', 'r') as file:
+    with open(password_hash_dir, 'r') as file:
         for line in file:
             password_hash = line
 
--- Trabalho3/main.py
+++ Trabalho3/main.py
@@ -1,16 +1,21 @@
 from functions import *
+import os
 
 def main():
     correct_password = 'teste'
 
-    with open('password_hash.txt', 'w') as file:
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    password_hash_dir = os.path.join(current_dir, 'password_hash.txt')
+    password_entry_dir = os.path.join(current_dir, 'password_entry.txt')
+
+    with open(password_hash_dir, 'w') as file:
         file.write(hash_password(correct_password))
 
-    with open('password_entry.txt', 'r') as file:
+    with open(password_entry_dir, 'r') as file:
         for line in file:
             print(f'A senha presente no arquivo de texto é: {line}')
 
-    if verify_password():
+    if verify_password(password_hash_dir, password_entry_dir):
         print('Senha correta!')
     else:
         print('Senha incorreta!')
